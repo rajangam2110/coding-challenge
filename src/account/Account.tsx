3de6f8df6fa40fa7c47f63a1bc74e5914537ad84
Rajangam Ganesan <rajangam@parrotanalytics.com>
2021-03-10 18:30:58 +1300

    Saas support added and created Account fuction class

---
+++ src/account/Account.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './styles.module.scss'
+
+/**
+ * Account Function to calculate some account metrics
+ * 
+ */
+
+export const Account = () => {
+    return (
+      <div className={styles.accountContainer}>        
+      </div>
+    );
+};
